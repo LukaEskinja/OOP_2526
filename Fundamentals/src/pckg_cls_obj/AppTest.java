7a0765eb98d1065fd5725620ac23738732be2b89
LukaEskinja <leskinja25@unizd.hr>
2026-03-11 00:12:50 +0100

    Edits

--- Fundamentals/src/pckg_cls_obj/AppTest.java
+++ Fundamentals/src/pckg_cls_obj/AppTest.java
@@ -1,13 +1,22 @@
 package pckg_cls_obj;
 
 public class AppTest {
+    public static void main(String[] args) {
+        Student s1 = new Student("Luka", "Eskinja", 13);
+        Student s2 = new Student("John", "Snow", 14);
 
-    public static void main(String[] args){
-        Student s1 = new Student("Petar","Petreić",12);
-        Student s2 = new Student("Franka","Franić",43);
         s1.subscribeToStudentService();
         s2.subscribeToStudentService();
 
-    }
+        System.out.println(s1);
 
+        Hlace h1 = new Hlace("Levis 501");
+        h1.koristeneHlace();
+
+        Hlace h2;
+        h2 = h1.nekakoCiste(s1);
+
+        System.out.println(h1);
+        System.out.println(h2);
+    }
 }
